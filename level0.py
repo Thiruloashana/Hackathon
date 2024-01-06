6751a4fd40566bbbd10b6982c052bfd293fb485f
unknown <21PT32@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 11:00:25 +0530

    lvl0 code struc

--- level0.py
+++ level0.py
@@ -8,8 +8,6 @@ rest={}
 n_ndist={}
 index=0
 for key,value in data.items():
-    print(key,value)
-
     n_neigh=data['n_neighbourhoods']
     print()
 
@@ -30,9 +28,28 @@ print("R_NDIST")
 print(r_ndist)
 print("N_NDIST")
 for key,value in n_ndist.items():
-    print(n_ndist[key]['distances'])
-
-visited={}
-
-
+    print(n_ndist[key]['distances']) 
 f.close()
+
+def traverse(visited,min_ind):
+    for i in range(n_neigh):
+        for j,k in n_ndist.items():
+            if j==min_ind:
+                for z in range(len(n_ndist[j]['distances'])):
+                    m=min(n_ndist[j]['distances'][z])
+                    m_ind=n_ndist[j]['distances'][z].index(m)
+                    while m_ind in visited:
+                        n_ndist[j]['distances'][z].remove(m)
+                        m=min(n_ndist[j]['distances'][z])     
+                        m_ind=n_ndist[j]['distances'][z].index(m)                    
+                    visited.append(m_ind)
+                    traverse(visited,m_ind)
+                                     
+
+
+visited=[]
+minn=min(r_ndist)
+min_ind=r_ndist.index(minn)
+visited.append(min_ind)
+#traverse(visited,min_ind)
+print(n_ndist[j]['distances'][0])
